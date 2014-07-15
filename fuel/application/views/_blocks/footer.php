0e6e085dd000d1b15cbb8c4122d6fb3d1c4b49fa
Russ Brown <poa02rb@yahoo.co.uk>
2014-07-15 18:33:32 +0100

    initial commit

---
+++ fuel/application/views/_blocks/footer.php
@@ -0,0 +1,12 @@
+			</div>
+	</div>
+	<div class="wrapper">
+	<footer class="row footer">
+		<p class="colophon"> &copy; <?php echo date("Y"); ?> Moorgate Motor Company, All Rights Reserved.</p>
+	</footer>
+</div>
+
+<?php echo js('main').js($js); ?>
+
+</body>
+</html>
