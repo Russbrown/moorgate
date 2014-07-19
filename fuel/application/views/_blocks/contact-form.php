f7dfbf777c6568ec81afadcd2b3e5f09f11fe8ea
Russ Brown <poa02rb@yahoo.co.uk>
2014-07-19 22:19:01 +0100

    Contact form

---
+++ fuel/application/views/_blocks/contact-form.php
@@ -0,0 +1,8 @@
+<form class="form sidebar text-padding">
+	<h2>Contact Us</h2>
+	<input type="text" name="name" placeholder="name" required />
+	<input type="tel" name="phone" placeholder="phone" required />
+	<input type="email" name="email" placeholder="email" required />
+	<textarea name="message" placeholder="message" required> </textarea>
+	<input type="submit" value="Send"/>
+</form>
--- fuel/application/views/home.php
+++ fuel/application/views/home.php
@@ -1,2 +1,3 @@
 <?php $this->load->view('_blocks/gallery') ?>
+<?php $this->load->view('_blocks/contact-form') ?>
 <?php $this->load->view('_blocks/home-content') ?>
