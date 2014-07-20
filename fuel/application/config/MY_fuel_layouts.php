ac3a761ee7e868bd895a56aa8444c6f44265b5a4
Russ Brown <poa02rb@yahoo.co.uk>
2014-07-20 15:17:57 +0100

    CMS Content page layout

--- fuel/application/config/MY_fuel_layouts.php
+++ fuel/application/config/MY_fuel_layouts.php
@@ -16,8 +16,6 @@ $config['layouts']['main'] = array(
 	// 'class'		=> 'Main_layout',
 	// 'filepath' => 'libraries',
 	// 'filename' => 'Main_layout.php',
-	
-	
 	'fields'	=> array(
 		'Header' => array('type' => 'fieldset', 'label' => 'Header', 'class' => 'tab'),
 		'page_title' => array('label' => lang('layout_field_page_title')),
@@ -26,6 +24,22 @@ $config['layouts']['main'] = array(
 		'Body' => array('type' => 'fieldset', 'label' => 'Body', 'class' => 'tab'),
 		'heading' => array('label' => lang('layout_field_heading')),
 		'body' => array('label' => lang('layout_field_body'), 'type' => 'textarea', 'description' => lang('layout_field_body_description')),
+		'body_class' => array('label' => lang('layout_field_body_class')),
+	)
+);
+
+$config['layouts']['content_page'] = array(
+	'fields'	=> array(
+		'Header' => array('type' => 'fieldset', 'label' => 'Header', 'class' => 'tab'),
+		'page_title' => array('label' => lang('layout_field_page_title')),
+		'meta_description' => array('label' => lang('layout_field_meta_description')),
+		'meta_keywords' => array('label' => lang('layout_field_meta_keywords')),
+		'Body' => array('type' => 'fieldset', 'label' => 'Body', 'class' => 'tab'),
+		'page_img' => array('label' => lang('layout_field_page_image')),
+		'heading_left_column' => array('label' => lang('layout_field_heading_left_column')),
+		'heading_right_column' => array('label' => lang('layout_field_heading_right_column')),
+		'body_left_column' => array('label' => lang('layout_field_body_left_column'), 'type' => 'textarea', 'description' => lang('layout_field_body_left_column_description')),
+		'body_right_column' => array('label' => lang('layout_field_body_right_column'), 'type' => 'textarea', 'description' => lang('layout_field_body_right_column_description')),				
 		'body_class' => array('label' => lang('layout_field_body_class')),
 	)
 );
---
+++ fuel/application/views/_layouts/content_page.php
@@ -0,0 +1,22 @@
+	<?php $this->load->view('_blocks/header')?>
+	
+	<div class="interaction-gallery">
+		<img src="/assets/images/<?= fuel_var('page_img'); ?>" class="active"/>
+	</div>
+	
+	<section id="main_inner">
+		<div class="main-column">
+			<div class="fifty text-padding has-image-right">
+				<h2><? echo fuel_var('heading_left_column'); ?></h2>
+				<? echo fuel_var('body_left_column'); ?>
+			</div>	
+			<div class="fifty text-padding has-image-right">
+				<h2><? echo fuel_var('heading_right_column'); ?></h2>
+				<? echo fuel_var('body_right_column'); ?>
+			</div>
+		</div>
+	</section>
+
+	<?php $this->load->view('_blocks/contact-form') ?>
+		
+	<?php $this->load->view('_blocks/footer')?>
