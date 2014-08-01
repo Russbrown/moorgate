2d7c7d1ff5e4ad2bf6f189c77b16fe1b8c3a48af
Russ Brown <poa02rb@yahoo.co.uk>
2014-08-01 11:24:33 +0100

    Contact form now working

--- fuel/application/controllers/enquiry.php
+++ fuel/application/controllers/enquiry.php
@@ -10,29 +10,58 @@ class Enquiry extends CI_Controller {
 	 * @author Russ Brown russ.brown@stoneacre.co.uk
 	 */
 	function contactForm(){
-		// $this->load->library('mailgun/vendor/autoload');
-		# Include the Autoloader (see "Libraries" for install instructions)
-		// require 'vendor/autoload.php';
+		// # Include the Autoloader (see "Libraries" for install instructions)
+		// require APPPATH . '/libraries/vendor/autoload.php';
 		// use Mailgun\Mailgun;
 
-		# Instantiate the client.
+		// # Instantiate the client.
 		// $mgClient = new Mailgun('key-3ax6xnjp29jd6fds4gc373sgvjxteol0');
 		// $domain = "samples.mailgun.org";
 
 		// # Make the call to the client.
 		// $result = $mgClient->sendMessage($domain, array(
 		//     'from'    => 'Excited User <me@samples.mailgun.org>',
-		//     'to'      => 'Baz <poa02rb@yahoo.co.uk',
+		//     'to'      => 'Baz <baz@example.com>',
 		//     'subject' => 'Hello',
 		//     'text'    => 'Testing some Mailgun awesomness!'
 		// ));
-		// show thank you page.
-		// $page_init = array('location' => '/thanks');
-		// $this->load->module_library(FUEL_FOLDER, 'fuel_page', $page_init);
-		// $this->fuel_page->render();
+
+		//////
+		require APPPATH . '/libraries/Mailgun.php' ;
+
+		mailgun_init('key-afy6amxoo2fnj$u@mc');
+
+		// // Plain text message
+		// MailgunMessage::send_text("me@samples.mailgun.org", 
+		//                           "'John Doe' <you@mailgun.info>",
+		//                           "Hello text PHP API!",
+		//                           "Hi!\nI am sending a message using Mailgun PHP API");
+
+		// // Plain text message + tag
+		// MailgunMessage::send_text("me@samples.mailgun.org", 
+		//                           "'John Doe' <you@mailgun.info>",
+		//                           "Hello text PHP API + tag!",
+		//                           "Hi!\nI am sending a message using Mailgun PHP API",
+		//                           "",
+		//                           array("headers" => array(MAILGUN_TAG => "sample_text_php")));
+
+		// MIME message
+		$rawMime = 
+		    "X-Priority: 1 (Highest)\n".
+		    "X-Mailgun-Tag: sample_raw_php\n".
+		    "Content-Type: text/plain;charset=UTF-8\n".    
+		    "From: me@samples.mailgun.org\n".
+		    "To: you@mailgun.info\n".
+		    "Subject: Hello raw PHP API!\n".
+		    "\n".
+		    "This message is sent by Mailgun PHP API";
+		MailgunMessage::send_raw("me@samples.mailgun.org", "you@mailgun.info", $rawMime); 
+
+		echo "Messages sent\n";
 	}
 
 	function contact(){
+		// $this->load->library('url');
 		$this->load->library('email');
 		$this->load->helper('inflector');
 		$this->load->model('fuel_data');
@@ -53,10 +82,7 @@ class Enquiry extends CI_Controller {
 		    // Generate error
 				echo('error');
 		}
-		// show thank you page.
-		// $page_init = array('location' => '/service/volvo-form-thanks', 'layout'=>'forms');
-		// $this->load->module_library(FUEL_FOLDER, 'fuel_page', $page_init);
-		// $this->fuel_page->render();
+		redirect('thanks');
 	}
 
 }
---
+++ fuel/application/views/thanks.php
@@ -0,0 +1,5 @@
+<?php $this->load->view('_blocks/gallery') ?>
+<div class="text-padding">
+	<h2> Thanks for getting in contact!</h2>
+	<p >We will get back to you as soon as possible.</p>
+</div>
