6f90b25839d3f5a0b5f9f44f9e2ece695db27043
Russ Brown <poa02rb@yahoo.co.uk>
2014-08-22 09:15:02 +0100

    labels added to contact form

--- fuel/application/views/_blocks/contact-form.php
+++ fuel/application/views/_blocks/contact-form.php
@@ -1,8 +1,12 @@
 <form action="enquiry/contact" method="POST" class="form sidebar text-padding">
 	<h2>Contact Us</h2>
+	<label for="name">Name</label>
 	<input type="text" name="name" placeholder="name" required />
+	<label for="phone">Phone Number</label>
 	<input type="tel" name="phone" placeholder="phone" required />
+	<label for="email">Email Address</label>
 	<input type="email" name="email" placeholder="email" required />
+	<label for="message">Your Message</label>
 	<textarea name="message" placeholder="message" required> </textarea>
 	<input type="submit" value="Send"/>
 </form>
