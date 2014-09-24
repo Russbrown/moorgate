30b240e42e1921cef1bbc8506def1f5aaa8386e7
Russ Brown <poa02rb@yahoo.co.uk>
2014-09-24 10:19:06 +0100

    vehicle sold style

--- fuel/application/views/_blocks/vehicle-sold.php
+++ fuel/application/views/_blocks/vehicle-sold.php
@@ -3,12 +3,12 @@
 
 <div class="vehicles">
 	<?php foreach ($vehicles as $vehicle) { ?>
-	<div class="vehicle sold">
+	<div class="vehicle">
 			
 			<div class="header"><h2><?= $vehicle->make . ' ' . $vehicle->model ?></h2></div>
 			<div class="body">
 				<img src="assets/images/<?= $vehicle->vehicle_1_img ?>" class="vehicle-img" />
-				<div class="vehicle-details">
+				<div class="vehicle-details sold">
 					<div class="item"><span class="label">REGISTRATION:</span><?= $vehicle->registration ?></div>
 					<div class="item"><span class="label">MILEAGE:</span><?= $vehicle->mileage ?></div>
 					<div class="item"><span class="label">FUEL:</span><?= $vehicle->fuel_type ?></div>
