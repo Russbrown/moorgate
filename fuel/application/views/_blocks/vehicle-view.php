efebd823cc78d0e9862113d7ba702881f6cb68ba
Russ Brown <poa02rb@yahoo.co.uk>
2014-10-02 10:42:06 +0100

    styling for car view

--- fuel/application/views/_blocks/vehicle-view.php
+++ fuel/application/views/_blocks/vehicle-view.php
@@ -2,7 +2,15 @@
 <div class="vehicle vehicle-view">		
 		<div class="header"><h2><?= $vehicle[0]->make . ' ' . $vehicle[0]->model ?></h2></div>
 		<div class="body">
-			<img src="/assets/images/<?= $vehicle[0]->vehicle_1_img ?>" class="vehicle-img" />
+			<div class="vehicle-images">
+
+				<img src="/assets/images/<?= $vehicle[0]->vehicle_1_img ?>" class="vehicle-img active" />
+
+				<?php if (isset($vehicle[0]->vehicle_2_img)) { ?>
+					<img src="/assets/images/<?= $vehicle[0]->vehicle_2_img ?>" class="vehicle-img" />
+				<?php	} ?>
+
+			</div>
 			<div class="vehicle-details">
 				<div class="item"><span class="label">REGISTRATION:</span><?= $vehicle[0]->registration ?></div>
 				<div class="item"><span class="label">MILEAGE:</span><?= $vehicle[0]->mileage ?></div>
@@ -13,3 +21,4 @@
 			</div>
 		</div>
 </div>
+
