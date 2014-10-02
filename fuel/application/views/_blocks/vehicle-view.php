28d5b0cb69cc1c01c9064ea9a7fbe8b18c4ea84b
Russ Brown <poa02rb@yahoo.co.uk>
2014-10-02 11:27:36 +0100

    click through to vehicles added

--- fuel/application/views/_blocks/vehicle-view.php
+++ fuel/application/views/_blocks/vehicle-view.php
@@ -1,27 +1,59 @@
 
 <div class="vehicle vehicle-view">		
 		<div class="header"><h2><?= $vehicle[0]->make . ' ' . $vehicle[0]->model ?></h2></div>
-		<div class="body">
-			<div class="vehicle-images gallery interaction-gallery">
+			<div class="body">
+				<div class="vehicle-images gallery interaction-gallery">
 
-				<div class="gallery__nav nav__left"><</div>
-				<div class="gallery__nav nav__right">></div>
+					<div class="gallery__nav nav__left"><</div>
+					<div class="gallery__nav nav__right">></div>
 
-				<img src="/assets/images/<?= $vehicle[0]->vehicle_1_img ?>" class="vehicle-img active" />
+					<img src="/assets/images/<?= $vehicle[0]->vehicle_1_img ?>" class="vehicle-img active" />
 
-				<?php if (isset($vehicle[0]->vehicle_2_img)) { ?>
-					<img src="/assets/images/<?= $vehicle[0]->vehicle_2_img ?>" class="vehicle-img" />
-				<?php	} ?>
+					<?php if (isset($vehicle[0]->vehicle_2_img)) { ?>
+						<img src="/assets/images/<?= $vehicle[0]->vehicle_2_img ?>" class="vehicle-img" />
+					<?php	} ?>
 
+					<?php if (isset($vehicle[0]->vehicle_3_img) && $vehicle[0]->vehicle_3_img != '') { ?>
+						<img src="/assets/images/<?= $vehicle[0]->vehicle_3_img ?>" class="vehicle-img" />
+					<?php	} ?>
+
+					<?php if (isset($vehicle[0]->vehicle_4_img) && $vehicle[0]->vehicle_3_img != '') { ?>
+						<img src="/assets/images/<?= $vehicle[0]->vehicle_4_img ?>" class="vehicle-img" />
+					<?php	} ?>
+
+					<?php if (isset($vehicle[0]->vehicle_5_img) && $vehicle[0]->vehicle_3_img != '') { ?>
+						<img src="/assets/images/<?= $vehicle[0]->vehicle_5_img ?>" class="vehicle-img" />
+					<?php	} ?>
+
+					<?php if (isset($vehicle[0]->vehicle_6_img) && $vehicle[0]->vehicle_3_img != '') { ?>
+						<img src="/assets/images/<?= $vehicle[0]->vehicle_6_img ?>" class="vehicle-img" />
+					<?php	} ?>
+
+					<?php if (isset($vehicle[0]->vehicle_7_img) && $vehicle[0]->vehicle_3_img != '') { ?>
+						<img src="/assets/images/<?= $vehicle[0]->vehicle_7_img ?>" class="vehicle-img" />
+					<?php	} ?>
+
+					<?php if (isset($vehicle[0]->vehicle_8_img) && $vehicle[0]->vehicle_3_img != '') { ?>
+						<img src="/assets/images/<?= $vehicle[0]->vehicle_8_img ?>" class="vehicle-img" />
+					<?php	} ?>
+
+					<?php if (isset($vehicle[0]->vehicle_9_img) && $vehicle[0]->vehicle_3_img != '') { ?>
+						<img src="/assets/images/<?= $vehicle[0]->vehicle_9_img ?>" class="vehicle-img" />
+					<?php	} ?>
+
+					<?php if (isset($vehicle[0]->vehicle_10_img) && $vehicle[0]->vehicle_3_img != '') { ?>
+						<img src="/assets/images/<?= $vehicle[0]->vehicle_10_img ?>" class="vehicle-img" />
+					<?php	} ?>
+
+				</div>
+				<div class="vehicle-details">
+					<div class="item"><span class="label">REGISTRATION:</span><?= $vehicle[0]->registration ?></div>
+					<div class="item"><span class="label">MILEAGE:</span><?= $vehicle[0]->mileage ?></div>
+					<div class="item"><span class="label">FUEL:</span><?= $vehicle[0]->fuel_type ?></div>
+					<div class="item"><span class="label">SPECIFICATION:</span><?= $vehicle[0]->spec ?></div>
+					<div class="price">£<?= $vehicle[0]->price ?></div>	
+					<div class="contact"><?= fuel_var('phone') ?></div>				
+				</div>
 			</div>
-			<div class="vehicle-details">
-				<div class="item"><span class="label">REGISTRATION:</span><?= $vehicle[0]->registration ?></div>
-				<div class="item"><span class="label">MILEAGE:</span><?= $vehicle[0]->mileage ?></div>
-				<div class="item"><span class="label">FUEL:</span><?= $vehicle[0]->fuel_type ?></div>
-				<div class="item"><span class="label">SPECIFICATION:</span><?= $vehicle[0]->spec ?></div>
-				<div class="price">£<?= $vehicle[0]->price ?></div>	
-				<div class="contact"><?= fuel_var('phone') ?></div>				
-			</div>
-		</div>
 </div>
 
--- fuel/application/views/_blocks/vehicle.php
+++ fuel/application/views/_blocks/vehicle.php
@@ -5,19 +5,21 @@
 <div class="vehicles">
 	<?php foreach ($vehicles as $vehicle) { ?>
 	<div class="vehicle">
-			
+
 			<div class="header"><h2><?= $vehicle->make . ' ' . $vehicle->model ?></h2></div>
-			<div class="body">
-				<img src="/assets/images/<?= $vehicle->vehicle_1_img ?>" class="vehicle-img" />
-				<div class="vehicle-details">
-					<div class="item"><span class="label">REGISTRATION:</span><?= $vehicle->registration ?></div>
-					<div class="item"><span class="label">MILEAGE:</span><?= $vehicle->mileage ?></div>
-					<div class="item"><span class="label">FUEL:</span><?= $vehicle->fuel_type ?></div>
-					<div class="item"><span class="label">SPECIFICATION:</span><?= $vehicle->spec ?></div>
-					<div class="price">£<?= $vehicle->price ?></div>	
-					<div class="contact"><?= fuel_var('phone') ?></div>				
+			<a href="/stock/view/<?= $vehicle->id ?>">
+				<div class="body">
+					<img src="/assets/images/<?= $vehicle->vehicle_1_img ?>" class="vehicle-img" />
+					<div class="vehicle-details">
+						<div class="item"><span class="label">REGISTRATION:</span><?= $vehicle->registration ?></div>
+						<div class="item"><span class="label">MILEAGE:</span><?= $vehicle->mileage ?></div>
+						<div class="item"><span class="label">FUEL:</span><?= $vehicle->fuel_type ?></div>
+						<div class="item"><span class="label">SPECIFICATION:</span><?= $vehicle->spec ?></div>
+						<div class="price">£<?= $vehicle->price ?></div>	
+						<div class="contact"><?= fuel_var('phone') ?></div>				
+					</div>
 				</div>
-			</div>
+			</a>
 
 		</div>
 	<?php } ?>
